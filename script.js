dd5a1e49b58b5abf2b19555c2e5e179197fb81a9
mbatha-17 <jullymbatha17@gmail.com>
2024-03-18 15:35:57 +0300

    created form

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const name= document.getElementById ('name')
-const email=document.getElementById ('email')
+const password=document.getElementById ('password')
 const form=document.getElementById ('form')
 const errorElement=document.getElementById ('error')
 
-form.addEventListener('submit'),(e)=>{
+form.addEventListener{"submit",(e)=>{
     let messages=[]
 }
 if (Name.value===''||Name.value==null){
     messages.push(Nameisrequired)
+}
+if(password.value.length <6){
+    messages.push(passwordmustbelongerthan6characters)
 }
 
 if (messages.length >0){
     e.preventDefault()
     errorElement.innerText=messages.join(',')
 }
+}   
